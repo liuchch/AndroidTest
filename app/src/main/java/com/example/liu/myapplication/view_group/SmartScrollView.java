5c4dea873a12bf31b466f27dfc00cc3999de59db
liu <andy66@lonlife.cn>
2018-10-19 17:54:29 +0800

    创建SmartScrollView

---
+++ app/src/main/java/com/example/liu/myapplication/view_group/SmartScrollView.java
@@ -0,0 +1,26 @@
+package com.example.liu.myapplication.view_group;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.ViewGroup;
+
+public class SmartScrollView extends ViewGroup {
+
+    public SmartScrollView(Context context) {
+        super(context);
+    }
+
+    public SmartScrollView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public SmartScrollView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+
+    }
+}
